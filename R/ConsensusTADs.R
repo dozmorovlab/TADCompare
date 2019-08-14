620e0e075ade5f6b55dfa8e85634ba8928951e72
cresswellkg <cresswellkg@vcu.edu>
2019-08-14 10:24:50 -0400

    fully seperate helper functions

--- R/ConsensusTADs.R
+++ R/ConsensusTADs.R
@@ -39,12 +39,6 @@
 ConsensusTADs = function(cont_mats, resolution,
                       z_thresh = 2, window_size = 15, gap_thresh = .8) {
 
-  require(PRIMME)
-  require(Matrix)
-  require(dplyr)
-  require(cluster)
-  require(softImpute)
-
   #Calculate boundary scores
   bound_scores = lapply(seq_len(length(cont_mats)), function(x) {
 
@@ -66,7 +60,7 @@ ConsensusTADs = function(cont_mats, resolution,
   colnames(score_frame)[1] = "Sample"
   base_sample = score_frame %>% filter(Sample == "Sample 1")
 
-  #Get differences
+  #Get differences in boundary scores
 
   score_frame = score_frame %>% dplyr::group_by(Sample)  %>%
     dplyr::mutate(Diff_Score = (base_sample$Boundary-Boundary)) %>%
@@ -97,7 +91,7 @@ ConsensusTADs = function(cont_mats, resolution,
   #Get median of boundary scores for each row
 
   TAD_Frame = TAD_Frame %>%
-    mutate(Consensus_Score =
+    dplyr::mutate(Consensus_Score =
     apply(TAD_Frame %>% dplyr::select(-Coordinate) %>% as.matrix(.)
           ,1, median))
 
@@ -106,160 +100,3 @@ ConsensusTADs = function(cont_mats, resolution,
 }
 
 
-Single_Dist = function(cont_mat1, resolution, window_size = 25, gap_thresh = .8) {
-
-  require(PRIMME)
-  require(Matrix)
-  require(dplyr)
-  require(cluster)
-
-  #Set maximize size of sliding window
-
-  #resolution = as.numeric(colnames(cont_mat1)[2])-as.numeric(colnames(cont_mat1)[1])
-  window_size = window_size
-
-  #Remove full gaps from matrices
-
-  non_gaps = which(colSums(cont_mat1) !=0)
-
-  cont_mat_filt = cont_mat1[non_gaps,non_gaps]
-
-  max_end = window_size
-
-  start = 1
-  end = max_end
-
-  end_loop = 0
-
-  if ( (end+window_size)>nrow(cont_mat_filt)) {
-    end = nrow(cont_mat_filt)
-  }
-
-  point_dists1 = rbind()
-
-  while (end_loop == 0) {
-    sub_filt1 = cont_mat_filt[start:end, start:end]
-
-    Per_Zero1 = (colSums(sub_filt1 ==0)/nrow(sub_filt1))>gap_thresh
-
-    sub_filt1 = sub_filt1[!Per_Zero1, !Per_Zero1]
-
-    #sub_filt1[sub_filt1==0] =NA
-
-    if ((length(sub_filt1) == 0) | (length(sub_filt1) == 1)) {
-      if (end == nrow(cont_mat1)) {
-        end_loop = 1
-      }
-
-      start = end
-      end = end+max_end
-
-      if ( (end + max_end) >nrow(cont_mat_filt)) {
-        end = nrow(cont_mat_filt)
-      }
-
-      if (start == end | start>nrow(cont_mat_filt)) {
-        end_loop = 1
-      }
-      next
-    }
-
-    #imputed = softImpute(sub_filt1)
-
-    #sub_filt1 = complete(sub_filt1, imputed)
-
-    #Creating the normalized laplacian
-
-    dr1 = rowSums(abs(sub_filt1))
-
-    Dinvsqrt1 = diag((1/sqrt(dr1+2e-16)))
-
-    P_Part1 = crossprod(as.matrix(sub_filt1), Dinvsqrt1)
-    sub_mat1 = crossprod(Dinvsqrt1, P_Part1)
-
-    #sub_mat = crossprod(diag(dr^-(1/2)), as.matrix(sub_filt))
-
-    colnames(sub_mat1) = colnames(sub_filt1)
-
-    #Find gaps at 2mb and remove
-
-
-    #Remove gaps if true
-
-    # if (length(sub_gaps) != 0) {
-    #   sub_mat = sub_mat[!sub_gaps, !sub_gaps]
-    # }
-
-
-    #Get first two eigenvectors
-
-    Eigen1 = eigs_sym(sub_mat1, NEig = 2)
-
-    eig_vals1 = Eigen1$values
-    eig_vecs1 = Eigen1$vectors
-
-    #Get order of eigenvalues from largest to smallest
-
-    large_small1 = order(-eig_vals1)
-
-    eig_vals1 = eig_vals1[large_small1]
-    eig_vecs1 = eig_vecs1[,large_small1]
-
-    #Normalize the eigenvectors
-
-    norm_ones = sqrt(dim(sub_mat1)[2])
-
-    for (i in 1:dim(eig_vecs1)[2]) {
-      eig_vecs1[,i] = (eig_vecs1[,i]/sqrt(sum(eig_vecs1[,i]^2)))  * norm_ones
-      if (eig_vecs1[1,i] !=0) {
-        eig_vecs1[,i] = -1*eig_vecs1[,i] * sign(eig_vecs1[1,i])
-      }
-    }
-
-
-    eps = 2.2204e-16
-
-    n = dim(eig_vecs1)[1]
-    k = dim(eig_vecs1)[2]
-
-    #Project eigenvectors onto a unit circle
-
-    vm1 = matrix(kronecker(rep(1,k), as.matrix(sqrt(rowSums(eig_vecs1^2)))),n,k)
-    eig_vecs1 = eig_vecs1/vm1
-
-
-    #Get distance between points on circle
-
-    point_dist1 = sqrt(rowSums( (eig_vecs1-rbind(NA,eig_vecs1[-nrow(eig_vecs1),]))^2  ))
-
-    #Get number of rows without gap threshold of contacts and make NA
-
-    # point_dist1[Per_Zero1] = NA
-    #
-    # point_dist1[which(Per_Zero1)+1] = NA
-
-
-    point_dist1 = cbind( match(colnames(sub_mat1),colnames(cont_mat1) ) , as.numeric(colnames(sub_mat1)), point_dist1)
-
-    point_dists1 = rbind(point_dists1, point_dist1[-1,])
-
-    if (end == nrow(cont_mat1)) {
-      end_loop = 1
-    }
-
-    start = end
-    end = end+max_end
-
-    if ( (end + max_end) >nrow(cont_mat_filt)) {
-      end = nrow(cont_mat_filt)
-    }
-
-    if (start == end | start>nrow(cont_mat_filt)) {
-      end_loop = 1
-    }
-  }
-
-  point_dists1 = as.data.frame(point_dists1)
-  colnames(point_dists1) = c("Index", "Coordinate","Boundary")
-  return(point_dists1 = point_dists1)
-}
