6054e00d0268e514992671ba9629bb087412bc78
cresswellkg <cresswellkg@vcu.edu>
2019-09-17 08:50:01 -0400

    fix warnings

---
+++ R/rao_chr22_prim.R
@@ -0,0 +1,9 @@
+#' Chromosome 22 combined intrachromosomal primary contact matrix from
+#' Rao et al. 2014.
+#'
+#' A 704x704 contact matrix from the GM12878 cell line (50kb Resolution)
+#'
+#' @format A data frame with 704 rows and 704 variables:
+#' \describe{
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE63525}
---
+++ R/time_mats.R
@@ -0,0 +1,9 @@
+#' Chromosome 22 time-varying contact matrices from Rao et al. 2014.
+#'
+#' Four 704x704 contact matrices representing 20,40,60,180 minutes since
+#' auxin treatment and removal from the HCT-116 cell line (50kb Resolution)
+#'
+#' @format A data frame with 704 rows and 704 variables:
+#' \describe{
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE104334}
