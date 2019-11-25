f95b8d08bb28d02d5dad220a677743cf354bc5a3
cresswellkg <cresswellkg@vcu.edu>
2019-11-24 20:30:16 -0500

    update data
    Former-commit-id: 8a2cd62b6dbb2a19171d17d01ebe3bcdd83af756

---
+++ R/GM12878.40kb.raw.chr_80_480.R
@@ -0,0 +1,9 @@
+#' Chromosome 2 contact matrix from Schmitt et al. 2016.
+#'
+#' A 1001x1001 contact matrix from the GM12878 cell line (40kb Resolution)
+#'
+#' @format A data frame with 1001 rows and 1001 variables:
+#' \describe{
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE87112}
+"GM12878.40kb.raw.chr_80_480"
