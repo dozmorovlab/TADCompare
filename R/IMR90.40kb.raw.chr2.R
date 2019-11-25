25b50b1007e344a0e69d5121e783a579c54115c4
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2019-11-25 08:38:59 -0500

    Fix description after renaming

--- R/IMR90.40kb.raw.chr2.R
+++ R/IMR90.40kb.raw.chr2.R
@@ -1,9 +1,10 @@
-#' Chromosome 2 contact matrix from Schmitt et al. 2016.
+#' A subset of chomosome 2 contact matrix, IMR90 cell line. 
 #'
-#' A 1001x1001 contact matrix from the IMR90 cell line (40kb Resolution)
+#' A 1001x1001 contact matrix from the IMR90 cell line, chr2:8000000-48000000,
+# 40kb Resolution, data from Schmitt et al. 2016.
 #'
 #' @format A data frame with 1001 rows and 1001 variables:
 #' \describe{
 #' }
 #' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE87112}
-"IMR90.40kb.raw.chr_80_480"
+"IMR90.40kb.raw.chr2"
