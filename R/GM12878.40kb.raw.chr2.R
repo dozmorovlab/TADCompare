dde26d24197d63c48862590c6b6da1ad7d64ad8e
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2019-11-25 08:54:52 -0500

    Fix typo

--- R/GM12878.40kb.raw.chr2.R
+++ R/GM12878.40kb.raw.chr2.R
@@ -1,7 +1,7 @@
 #' A subset of chomosome 2 contact matrix, GM12878 cell line. 
 #'
 #' A 1001x1001 contact matrix from the GM12878 cell line, chr2:8000000-48000000,
-# 40kb Resolution, data from Schmitt et al. 2016.
+#' 40kb Resolution, data from Schmitt et al. 2016.
 #'
 #' @format A data frame with 1001 rows and 1001 variables:
 #' \describe{
--- R/IMR90.40kb.raw.chr2.R
+++ R/IMR90.40kb.raw.chr2.R
@@ -1,7 +1,7 @@
 #' A subset of chomosome 2 contact matrix, IMR90 cell line. 
 #'
 #' A 1001x1001 contact matrix from the IMR90 cell line, chr2:8000000-48000000,
-# 40kb Resolution, data from Schmitt et al. 2016.
+#' 40kb Resolution, data from Schmitt et al. 2016.
 #'
 #' @format A data frame with 1001 rows and 1001 variables:
 #' \describe{
