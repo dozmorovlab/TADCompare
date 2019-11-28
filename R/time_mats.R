3d2f40698821df8f96fb083e36c0310de5e5d1be
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2019-11-28 09:02:28 -0500

    Fix help
    Former-commit-id: 5c7309e90e61df4c72fe0f7f85abec6918507c15

--- R/time_mats.R
+++ R/time_mats.R
@@ -1,6 +1,6 @@
-#' Chromosome 22 time-varying contact matrices from Rao et al. 2014.
+#' Chromosome 22 time-varying contact matrices from Rao et al. 2017.
 #'
-#' Four 704x704 contact matrices representing 20,40,60,180 minutes since
+#' Four 704x704 contact matrices representing 20, 40, 60, 180 minutes since
 #' auxin treatment and removal from the HCT-116 cell line (50kb Resolution)
 #'
 #' @format A data frame with 704 rows and 704 variables:
